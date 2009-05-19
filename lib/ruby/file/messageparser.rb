ad403a6e10dceb1fb18d993bd30028addab23820
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2009-05-20 00:25:39 +0200

    Parser Bug

---
+++ lib/ruby/file/messageparser.rb
@@ -0,0 +1,148 @@
+module Riddl
+  class File
+    class MessageParser
+      def initialize(doc,params,headers)
+        @doc = doc
+        @mist = params
+        @mistp = 0
+        @headers = headers
+      end
+
+      def check(name)
+        #{{{
+        @doc.find("/des:description/des:message[@name='#{name}']").each do |m|
+          m.find("des:header").each do |h|
+            return false unless header h
+          end
+          m.find("des:*[not(name()='header')]").each do |p|
+            return false unless send p.name.to_s, p
+          end
+        end  
+        true
+        #}}}
+      end
+
+      def oneOrMore(a)
+        tistp = @mistp
+        ncounter = 0
+        begin
+          counter,length = traverse_simple(a,true)
+          ncounter += 1 if counter == length
+        end while counter == length && @mistp < @mist.length
+        if ncounter > 0 
+          true
+        else  
+          @mistp = tistp
+          false
+        end  
+      end
+
+      def zeroOrMore(a)
+        ncounter = 0
+        begin
+          counter,length = traverse_simple(a,true)
+          ncounter += 1 if counter == length
+        end while counter == length && @mistp < @mist.length 
+        true
+      end
+
+      def choice(a)
+        a.find("des:*").each do |p|
+          return true if send p.name.to_s, p
+        end
+        false
+      end  
+      
+      def group(a)
+        tistp = @mistp
+        success = true
+        a.find("des:*").each do |p|
+          unless send p.name.to_s, p
+            success = false
+            break
+          end
+        end
+        if success
+          true
+        else  
+          @mistp = tistp
+          false
+        end  
+      end  
+
+      def optional(a)
+        tistp = @mistp
+        counter, length = traverse_simple(a)
+        if counter == 0 || counter == length
+          true
+        else  
+          @mistp = tistp
+          false
+        end  
+      end
+
+      def parameter(a)
+        #{{{
+        return false if @mistp >= @mist.length
+        b = @mist[@mistp]
+        if b.class == Riddl::Parameter::Simple && (a.attributes['fixed'] || a.attributes['type'])
+          if b.name == a.attributes['name']
+            @mistp += 1
+            return match_simple(a,b.value)
+          end
+        end
+        if b.class == Riddl::Parameter::Complex && a.attributes['mimetype']
+          #TODO
+          #wenn mimetype check handler
+        end  
+        false
+        #}}}
+      end
+      private :parameter
+      
+      def header(a)
+        #{{{
+        name = a.attributes['name'].upcase.sub(/\-/,'_')
+        if @headers.has_key?(name)
+          return match_simple(a,@headers[name])
+        end
+        false
+        #}}}
+      end
+      private :header
+
+      def traverse_simple(a,single_optional_protection=false)
+        tistp = @mistp
+        nodes = a.find("des:*")
+        counter = 0
+        lastname = ''
+        nodes.each do |p|
+          lastname = p.name.to_s 
+          counter += 1 if send lastname, p
+        end
+        if single_optional_protection && lastname == 'optional' && tistp == @mistp
+          [0,-1]
+        else  
+          [counter,nodes.length]
+        end 
+      end
+      
+      def match_simple(a,b)
+        #{{{
+        if a.attributes['fixed']
+          a.attributes['fixed'] == b
+        else  
+          value = XML::Smart::string("<check/>")
+          value.root.text = b
+          type = XML::Smart::string(CHECK)
+          data = type.root.children[0]
+          data.attributes['type'] = a.attributes['type']
+          a.children.each { |e| data.add(e) }
+          value.validate_against type
+        end  
+        #}}}
+      end
+      private :match_simple
+    end  
+  end    
+end
