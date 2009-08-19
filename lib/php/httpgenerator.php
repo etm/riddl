9ccb71518d96254ae6807db50f8c71bc1eb3c00c
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2009-08-19 22:32:18 +0200

    httpgenerator.php

--- lib/php/httpgenerator.php
+++ lib/php/httpgenerator.php
@@ -1,101 +1,103 @@
 <?
   class Riddl_HttpGenerator {
-    $BOUNDARY = "Time_is_an_illusion._Lunchtime_doubly_so.0xriddldata";
-    $EOL = "\r\n";
 
-    function __construct($params,$headers,$url) {
-      $this->url = parse_url($url);
+    private $BOUNDARY = "Time_is_an_illusion._Lunchtime_doubly_so.0xriddldata";
+    private $EOL = "\r\n";
+    private $BUFFERSIZE = 8192;
+
+    function __construct($params,$headers,$sock) {
+      $this->sock = $sock;
       $this->params = $params;
       $this->headers = $headers;
     }
 
-    def generate
-      if @params.class == Array && @params.length == 1
-        body(@params[0])
-      elsif @params.class == Riddl::Parameter::Simple || @params.class == Riddl::Parameter::Complex
-        body(@params)
-      elsif @params.class == Array && @params.length > 1
-        multipart
-      else
-        StringIO.new('r+b')
-      end  
+    function generate() {
+      if (is_array($this->params) && count($this->params) == 1) {
+        $this->body($this->params[0]);
+      } elseif (is_a($this->params,'RiddlParameterSimple') || is_a($this->params,'RiddlParameterComplex')) {
+        $this->body($this->params);
+      } elseif (is_array($this->params) && count($this->params) > 1)
+        $this->multipart();
+      }
+      fwrite($this->sock, $this->EOL);
     end
 
-    def body(r)
-      tmp = StringIO.new('r+b')
-      case r
-        when Riddl::Parameter::Simple
-          tmp.write r.value
-          @headers['Content-Type'] = 'text/riddl-data'
-          @headers['Content-Disposition'] = "riddl-data; name=\"#{r.name}\""
-        when Riddl::Parameter::Complex
-          tmp.write(r.value.respond_to?(:read) ? r.value.read : r.value)
-          @headers['Content-Type'] = r.mimetype
-          if r.filename.nil?
-            @headers['Content-ID'] = r.name
-          else
-            @headers['Content-Disposition'] = "riddl-data; name=\"#{r.name}\"; filename=\"#{r.filename}\""
-          end  
-      end
-      tmp.flush
-      tmp.rewind
-      tmp
-    end
-    private :body
+    private function body($r) {
+      if (is_a($r,'RiddlParameterSimple')) {
+        fwrite($this->sock, "Content-type: text/riddl-data" . $this->EOL);
+        fwrite($this->sock, "Content-length: " . $r->size . $this->EOL);
+        fwrite($this->sock, $this->EOL);
+        fwrite($this->sock, $r->value);
+        fwrite($this->sock, $this->EOL);
+      } elseif (is_a($this->params,'RiddlParameterComplex')) {
+        fwrite($this->sock, "Content-type: " . $r->mimetype . $this->EOL);
+        fwrite($this->sock, "Content-length: " . $r->size . $this->EOL);
+        if (is_null($r.filename)) {
+          fwrite($this->sock, "Content-ID: " . $r->name . $this->EOL);
+        } else {
+          fwrite($this->sock, "Content-Disposition: riddl-data; name=\"" . $r->name . "\"; filename=\"" . $r->filename . "\"" . $this->EOL);
+        }
+        fwrite($this->sock, $this->EOL);
+        if (is_resource($r->value) && (get_resource_type($r->value) == 'file')) {
+          $this->copy_content($r->value);
+        } elseif (is_buffer($r->value)) {
+          fwrite($this->sock, $r->value);
+        }
+        fwrite($this->sock, $this->EOL);
+      }  
+    }
 
-    def multipart
-      tmp = StringIO.new('r+b')
-      @headers['Content-Type'] = "multipart/mixed; boundary=\"#{BOUNDARY}\""
-      @params.each do |r|
-        case r
-          when Riddl::Parameter::Simple
-            tmp.write "--" + BOUNDARY + EOL
-            tmp.write "Content-Disposition: riddl-data; name=\"#{r.name}\"" + EOL
-            tmp.write EOL
-            tmp.write r.value
-            tmp.write EOL
-          when Riddl::Parameter::Complex
-            tmp.write "--" +  BOUNDARY + EOL
-            tmp.write "Content-Disposition: riddl-data; name=\"#{r.name}\""
-            tmp.write r.filename.nil? ? EOL : "; filename=\"#{r.filename}\"" + EOL
-            tmp.write "Content-Transfer-Encoding: binary" + EOL
-            tmp.write "Content-Type: " + r.mimetype + EOL
-            tmp.write EOL
-            tmp.write(r.value.respond_to?(:read) ? r.value.read : r.value)
-            tmp.write EOL
-        end   
-      end
-      tmp.write "--" + BOUNDARY + EOL
-      tmp.flush
-      tmp.rewind
-      tmp
-    end
-    private :multipart
-  
-  $sock = fsockopen("ssl://secure.example.com", 443, $errno, $errstr, 30);
-  if (!$sock) die("$errstr ($errno)\n");
+    private function copy_content($handle) {
+      while (!feof($handle)) {
+        fwrite($this->sock, fread($handle, $this->BUFFERSIZE));
+      }
+    }
+    
+    private function multipart($rs) {
+      fwrite($this->sock, "Content-type: multipart/mixed; boundary=\"" . $this->BOUNDARY . "\"" . $this->EOL);
+      fwrite($this->sock, $this->EOL);
+      foreach($rs as $r) {
+        if (is_a($r,'RiddlParameterSimple')) {
+          fwrite($this->sock, "--" . $this->BOUNDARY . $this->EOL);
+          fwrite($this->sock, "Content-Disposition: riddl-data; name=\"" . $r->name . "\"" . $this->EOL);
+          fwrite($this->sock, $this->EOL);
+          fwrite($this->sock, $r->value);
+          fwrite($this->sock, $this->EOL);
+        } elseif (is_a($this->params,'RiddlParameterComplex')) {
+          fwrite($this->sock, "--" . $this->BOUNDARY . $this->EOL);
+          fwrite($this->sock, "Content-Disposition: riddl-data; name=\"");
+          if (is_null($r->filename)) {
+            fwrite($this->sock, $this->EOL);
+          } else {
+            fwrite($this->sock, "; filename=\"" . $r->filename . "\"" . $this->EOL);
+          }
+          fwrite($this->sock, "Content-Transfer-Encoding: binary" . $this->EOL);
+          fwrite($this->sock, "Content-type: " . $r->mimetype . $this->EOL);
+          fwrite($this->sock, $this->EOL);
+          if (is_resource($r->value) && (get_resource_type($r->value) == 'file')) {
+            $this->copy_content($r->value);
+          } elseif (is_buffer($r->value)) {
+            fwrite($this->sock, $r->value);
+          }
+          fwrite($this->sock, $this->EOL);
+          fwrite($this->sock, "Content-type: " . $r->mimetype . $this->EOL);
+          fwrite($this->sock, "Content-length: " . $r->size . $this->EOL);
+          if (is_null($r.filename)) {
+            fwrite($this->sock, "Content-ID: " . $r->name . $this->EOL);
+          } else {
+            fwrite($this->sock, "Content-Disposition: riddl-data; name=\"" . $r->name . "\"; filename=\"" . $r->filename . "\"" . $this->EOL);
+          }
+          fwrite($this->sock, $this->EOL);
+          if (is_resource($r->value) && (get_resource_type($r->value) == 'file')) {
+            $this->copy_content($r->value);
+          } elseif (is_buffer($r->value)) {
+            fwrite($this->sock, $r->value);
+          }
+          fwrite($this->sock, $this->EOL);
+        }  
+      }
+      fwrite($this->sock, "--" . $this->BOUNDARY . $this->EOL);
+    }
 
-  $data = "foo=" . urlencode("Value for Foo") . "&bar=" . urlencode("Value for Bar");
-
-  fwrite($sock, "POST /form_action.php HTTP/1.0\r\n");
-  fwrite($sock, "Host: secure.example.com\r\n");
-  fwrite($sock, "Content-type: application/x-www-form-urlencoded\r\n");
-  fwrite($sock, "Content-length: " . strlen($data) . "\r\n");
-  fwrite($sock, "Accept: */*\r\n");
-  fwrite($sock, "\r\n");
-  fwrite($sock, "$data\r\n");
-  fwrite($sock, "\r\n");
-
-  $headers = "";
-  while ($str = trim(fgets($sock, 4096)))
-  $headers .= "$str\n";
-
-  echo "\n";
-
-  $body = "";
-  while (!feof($sock))
-  $body .= fgets($sock, 4096);
-
-  fclose($sock);
+  }
 ?>
-
