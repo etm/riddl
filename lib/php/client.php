11df4c5615d9f31c46a94819370621ce30145c9f
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2009-08-24 14:26:47 +0200

    client.php

--- lib/php/client.php
+++ lib/php/client.php
@@ -5,6 +5,7 @@
   require_once($includes . "/httpgenerator.php");
 
   class RiddlClient {
+    private $EOL = "\r\n";
     private $base;
     private $resource;
     private $debug;
@@ -35,41 +36,46 @@
           }
         }
       }
-      $this->riddl_it($params,$headers);
+      $this->riddl_it($type,$params,$headers);
     }
 
-    private function riddl_it() {
+    private function riddl_it($type,$params,$headers) {
       if (is_null($this->debug)) {
-        $g = new RiddlHttpGenerator($this->headers,$this->params,fopen('php://output','w'),'header');
+        $urlp = parse_url($http->base . $http->resource);
+        if (!isset($urlp['scheme']))
+          $urlp['scheme'] = 'http';
+        if (!isset($urlp['port'])) {
+          switch ($urlp['scheme']) {
+            case 'http':
+              $urlp['port'] = 80;
+              break;
+            case 'https':
+              $urlp['port'] = 443;
+              break;
+          }
+        }
+
+        $sock = fsockopen($urlp['scheme'] . '://' . $urlp['host'], $urlp['port'], $errno, $errstr, 30);
+        if (!$sock) die("$errstr ($errno)\n");
+        fwrite($sock, $type . " /" . $urlp['path'] . " HTTP/1.0" . $this->EOL);
+        fwrite($sock, "Host: " . $urlp['host'] . $this->EOL);
+        $g = new RiddlHttpGenerator($this->headers,$this->params,fopen($sock,'w'),'socket');
+
+        $headers = "";
+        while ($str = trim(fgets($sock, 4096)))
+        $headers .= "$str\n";
+
+        echo "\n";
+        $body = "";
+        while (!feof($sock))
+        $body .= fgets($sock, 4096);
+        fclose($sock);
+
       } else {
         $g = new RiddlHttpGenerator($this->headers,$this->params,fopen($this->debug,'w'),'socket');
       }
       $g->generate();
-      exit;
     }
   }
-
-#  $sock = fsockopen("ssl://secure.example.com", 443, $errno, $errstr, 30);
-#  if (!$sock) die("$errstr ($errno)\n");
-#
-#  $data = "foo=" . urlencode("Value for Foo") . "&bar=" . urlencode("Value for Bar");
-#  fwrite($sock, "POST /form_action.php HTTP/1.0\r\n");
-#  fwrite($sock, "Host: secure.example.com\r\n");
-#  fwrite($sock, "Content-type: application/x-www-form-urlencoded\r\n");
-#  fwrite($sock, "Content-length: " . strlen($data) . "\r\n");
-#  fwrite($sock, "Accept: */*\r\n");
-#  fwrite($sock, "\r\n");
-#  fwrite($sock, "$data\r\n");
-#  fwrite($sock, "\r\n");
-#
-#  $headers = "";
-#  while ($str = trim(fgets($sock, 4096)))
-#  $headers .= "$str\n";
-#
-#  echo "\n";
-#  $body = "";
-#  while (!feof($sock))
-#  $body .= fgets($sock, 4096);
-#  fclose($sock);
 
 ?>
