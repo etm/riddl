017c86deb58bb4dfc894648b7b8b636ce9545bf4
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2009-09-13 18:38:03 +0200

    rescue fix

--- examples/repository/iPhone/libs/rescue.rb
+++ examples/repository/iPhone/libs/rescue.rb
@@ -8,22 +8,23 @@ class RESCUE < Riddl::Implementation
       p "An error occurde on resource: groups/#{@r[2...5].join("/")}"
       return Riddl::Parameter::Complex.new("feed","text/html") do "An error (No. #{status}) occurde on resource: groups/#{@r[2...5].join("/")}" end
     end
-    html = div_(:id => 'resuce') do
+
+    html = div_(:id => 'rescue') do
       div_ :class => "toolbar" do
         h1_ @r.last
         a_ "Back", :class => "back button", :href => "#"
       end
       ul_ do
         pp "URI: Rescource: #{@r.join("/")}"
-        XML::Smart::string(res[0].value).find("//entry").each do |e|
+        XML::Smart::string(res[0].value.read).find("//entry").each do |e|
           id = e.find("string(id)")
           link = e.find("string(link)")
           li_ :class => "arrow" do
             a_ id, :href => "123/#{@r[1...4].join("/")}/#{id}"
           end
+        end  
       end
     end
     Riddl::Parameter::Complex.new("div","text/html",html)
   end
-end
 end
