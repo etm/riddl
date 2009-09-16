1c9cc41d715d92158606993ddad9cb410837cc29
Ralph Vigne <ralph@ralph-desktop.(none)>
2009-09-16 21:11:09 +0200

    iPhone: add and remove from wallet works, expect the reload of the wallet after removing an entry

--- examples/repository/iPhone/libs/wallet.rb
+++ examples/repository/iPhone/libs/wallet.rb
@@ -1,41 +1,33 @@
+class DeleteFromWallet < Riddl::Implementation
+  include MarkUSModule
+
+  def response
+    #pp @r
+    pp @p[0].value
+    #["08c5c293b7c28c0910ebc4848136e058", "wallet"]
+    # Create user dir if not exist
+    if File.exists?("user/#{@r.join("/")}/#{@p[0].value}") == false
+      puts "Directory: user/#{@r.join("/")}/#{@p[0].value} does not exist" 
+      @status = 410 # Conflict
+    else
+      FileUtils.rm_r("user/#{@r.join("/")}/#{@p[0].value}")
+    end
+  end
+end
+
 class AddToWallet < Riddl::Implementation
   include MarkUSModule
 
   def response
     #pp @r
+    #pp @p[0].value
     #["08c5c293b7c28c0910ebc4848136e058", "wallet"]
     # Create user dir if not exist
     if File.exists?("user/#{@r.join("/")}/#{@p[0].value}")
       puts "Directory: user/#{@r.join("/")}/#{@p[0].value} already exists" 
-#      @status = 409 # Conflict
-      Riddl::Parameter::Complex.new("html","text/html") do
-        div_ :id => 'rescue' do  
-          div_ :class => "toolbar" do
-            h1_ "Wallet"
-            a_ "Back", :class => "back button", :href => "#"
-          end
-          div_ :class => "message", :align=>"center" do
-            br_
-            br_
-            h3_ "Resouce is already in your wallet", :style=>"font-size: 24pt; color: #FFFFFF"
-          end
-        end
-      end
+      @status = 409 # Conflict
     else
       FileUtils.mkdir_p("user/#{@r.join("/")}/#{@p[0].value}")
-      Riddl::Parameter::Complex.new("html","text/html") do
-        div_ :id => 'rescue' do  
-          div_ :class => "toolbar" do
-            h1_ "Wallet"
-            a_ "Back", :class => "back button", :href => "#"
-          end
-          div_ :class => "message", :align=>"center" do
-            br_
-            br_
-            h3_ "Resource added to your wallet", :style=>"font-size: 24pt; color: #FFFFFF"
-          end
-        end
-      end
     end
   end
 end
@@ -49,7 +41,7 @@ class GetWallet < Riddl::Implementation
     if File.exist?("user/#{@r[0]}/wallet") == false
       p "Wallet does not exists"
       Riddl::Parameter::Complex.new("html","text/html") do
-        div_ :id => 'rescue' do  
+        div_ :id => 'wallet' do  
           div_ :class => "toolbar" do
             h1_ "Wallet"
             a_ "Back", :class => "back button", :href => "#"
@@ -64,7 +56,7 @@ class GetWallet < Riddl::Implementation
     else
     # Wallet is not empty
       Riddl::Parameter::Complex.new("html","text/html") do
-        div_ :id => 'rescue' do  
+        div_ :id => 'wallet' do  
           div_ :class => "toolbar" do
             h1_ "Wallet"
             a_ "Back", :class => "back button", :href => "#"
@@ -87,7 +79,10 @@ class GetWallet < Riddl::Implementation
       end
     else 
       x = dir.split("/")
-      li_ x[3...x.size-1].join("/")
+      li_ :style=>"vertical-align: center;" do
+        img_ :src=>"../js/custom/minusButton.png", :onclick=>"removeFromWallet('#{x[3...x.size-1].join("/")}', 'wallet')", :style=>"vertical-align: center;"
+        span_ x[3...x.size-1].join("/"), :style=>"display:inline; margin-left:10px;vertical-align: center;"
+      end
     end
   end
 end
