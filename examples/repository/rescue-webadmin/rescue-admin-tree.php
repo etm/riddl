090924e6e76eea5573012d0b6c65a00e1c19d3b8
Ralph Vigne <ralph@ralph-desktop.(none)>
2009-09-18 17:38:51 +0200

    RESCUE: service-details.rng and acording samples changed

--- examples/repository/rescue-webadmin/rescue-admin-tree.php
+++ examples/repository/rescue-webadmin/rescue-admin-tree.php
@@ -18,7 +18,8 @@
 <b>Repository:</b><br /><br />
 
 <?php 
-  $tree = new RESCUENavTree("myRepos", "http://sumatra.pri.univie.ac.at:9290:/", "Daten");
+//  $tree = new RESCUENavTree("myRepos", "http://sumatra.pri.univie.ac.at:9290:/", "Daten");
+  $tree = new RESCUENavTree("myRepos", "http://localhost:9290:/", "Daten");
   $tree->generate();
 ?>
 
