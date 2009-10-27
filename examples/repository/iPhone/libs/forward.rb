1415fd12a169662b178a76316ad5ef40bdddcc6d
Ralph Vigne <ralph@vigne.at>
2009-10-27 17:35:24 +0100

    removed iPhone from repo

--- examples/repository/iPhone/libs/forward.rb
+++
@@ -1,8 +0,0 @@
-require 'digest/md5'
-
-class Forward < Riddl::Implementation
-  def response
-    @headers << Riddl::Header.new("Location","/" + Digest::MD5.hexdigest(Time.now.to_s + Kernel.rand.to_s)+"/")
-    @status = 302
-  end
-end
