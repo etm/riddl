3e9963c18b6e98d5b5c09f86f84f7c122d4bc6a5
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2009-11-25 22:30:45 +0100

    missing files for flickr example

---
+++ examples/flickr/client.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+require '../../lib/ruby/client'
+require 'Helpers/flickr.rb'
+require 'pp'
+  
+fh = FlickrHelper.new("Flickr/")
+params = [ 
+  Riddl::Parameter::Simple.new("api_key", fh.api_key),
+  Riddl::Parameter::Simple.new("auth_token", fh.auth_token),
+  Riddl::Parameter::Complex.new("photo","image/jpeg",File.open('riddl.jpg','r'),'riddl.jpg'),
+  Riddl::Parameter::Simple.new("author", "Jürgen Mangler"),
+  Riddl::Parameter::Simple.new("title", "RIDDL Logo"),
+  Riddl::Parameter::Simple.new("description", "The official RIDDL logo, the first thing created for this project."),
+  Riddl::Parameter::Simple.new("tags", "RIDDL, REST, Composition, Evolution"),
+  Riddl::Parameter::Simple.new("longitude", 48.213736),
+  Riddl::Parameter::Simple.new("latitude", 16.357141),
+  Riddl::Parameter::Simple.new("is_public", 1),
+]
+params <<  Riddl::Parameter::Simple.new("api_sig", fh.sign(params))
+
+dflick = Riddl::Client.facade("declaration.xml")
+upload = dflick.resource("/upload")
+status, res = upload.post params
+
+puts status
+pp res
