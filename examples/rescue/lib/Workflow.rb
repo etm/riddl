996b00aec300e40d9767a6608d86270c0586cbc9
Ralph Vigne <ralph@vigne.at>
2010-04-21 21:54:41 +0200

    loop instead of cycle

--- examples/rescue/lib/Workflow.rb
+++ examples/rescue/lib/Workflow.rb
@@ -3,22 +3,22 @@ require 'xml/smart'
 require '../../lib/ruby/client'
 
 class Workflow
-  @__workflow = nil
-  @__riddl_client = nil
+  @workflow = nil
+  @riddl_client = nil
 
 
 
   def initialize(filename)
     load(filename)
-    @__riddl_client = Riddl::Client.new("http://localhost:9290/groups")
+    @riddl_client = Riddl::Client.new("http://localhost:9290/groups")
   end
 
   def load(filename)
-    @__workflow = XML::Smart.open(filename)
+    @workflow = XML::Smart.open(filename)
   end
 
   def resolve_calls()
-    calls = @__workflow.find("//flow:call[string(@service-operation)]", {"flow" => "http://rescue.org/ns/controlflow/0.2"})
+    calls = @workflow.find("//flow:call[string(@service-operation)]", {"flow" => "http://rescue.org/ns/controlflow/0.2"})
     calls.each do |call|
       endpoints= Hash.new
       inputs = Hash.new
@@ -42,7 +42,7 @@ class Workflow
   end
 
   def to_s
-    @__workflow.to_s
+    @workflow.to_s
   end
 
   private
@@ -58,7 +58,7 @@ class Workflow
     puts outputs.inspect
     puts '='*100
 
-    resource = @__workflow.find("//flow:endpoint[@id='#{endpoint}']", {"flow"=>"http://rescue.org/ns/controlflow/0.2"}).first.text.split("/")[0]
+    resource = @workflow.find("//flow:endpoint[@id='#{endpoint}']", {"flow"=>"http://rescue.org/ns/controlflow/0.2"}).first.text.split("/")[0]
     resp = do_riddl_call("#{resource}/operations/#{service_operation}", 200, [])
     
     block = XML::Smart.string(resp[0].value.read)
@@ -75,7 +75,7 @@ class Workflow
 
     flows = block.find("//domain:#{state_controlflow}/descendant::flow:*", {"domain"=>"http://rescue.org/ns/domain/0.2","flow"=>"http://rescue.org/ns/controlflow/0.2"})
     flows.each do |f|
-      f.attributes['id'] = "#{call_id}__#{f.attributes['id']}" if f.attributes.include?('id')
+      f.attributes['id'] = "#{call_id}#{f.attributes['id']}" if f.attributes.include?('id')
       f.attributes['endpoint'] = endpoints[f.attributes['endpoint'].to_sym] if f.attributes.include?('endpoint') and endpoints.include?(f.attributes['endpoint'].to_sym)
     end
     block.find("//flow:input",  {"flow"=>"http://rescue.org/ns/controlflow/0.2"}).each do |input|
@@ -88,7 +88,7 @@ class Workflow
   def resolve_message(message, params)
     puts message.parent.attributes['id']
     puts message.parent.attributes['endpoint']
-    resource = @__workflow.find("//flow:endpoint[@id='#{message.parent.attributes['endpoint']}']", {"flow"=>"http://rescue.org/ns/controlflow/0.2"}).first.text.split("/")[0]
+    resource = @workflow.find("//flow:endpoint[@id='#{message.parent.attributes['endpoint']}']", {"flow"=>"http://rescue.org/ns/controlflow/0.2"}).first.text.split("/")[0]
     puts "Message for #{resource}"
     resp = do_riddl_call(resource, 200, [Riddl::Parameter::Simple.new("input", "")])
     XML::Smart.string(resp[0].value.read).find("/rng:element/rng:element", {"rng"=>"http://relaxng.org/ns/structure/1.0"}).each do |param|
@@ -98,7 +98,7 @@ class Workflow
   end
 
   def do_riddl_call(resource, expectation, params)
-    status, resp = @__riddl_client.resource(resource).get params
+    status, resp = @riddl_client.resource(resource).get params
     puts "Error receiving information from #{resource} at endpoint:#{resource}" if status != expectation
     resp
   end
