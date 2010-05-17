9db70b923f36c9a97df1f8103e956ab844be010d
Ralph Vigne <ralph@vigne.at>
2010-05-17 17:29:31 +0200

    RESCUEv0.2: added testservice

---
+++ examples/rescue/Testservices/Cinema.php
@@ -0,0 +1,27 @@
+<?
+  require_once("./serversimple.php");
+  $s = new RiddlServerSimple();
+  $params = $s->request();
+#  print_r($params);
+
+  if(strcasecmp($_SERVER['REQUEST_METHOD'], "GET") == 0) { # A list of shows is returned 
+    # Expected Parameters (in order): title, datei, prefix_id
+    $title = $params[0]->value();
+    $date = $params[1]->value();
+    $prefix = $params[2]->value();
+    $number_of_shows = rand(1, 5);
+    $response = "<list_of_shows>\n";
+    for($i = 0; $i < $number_of_shows; $i++) {
+      $response .= "<show>\n";
+      $response .= "<show_id>". $prefix ."_" . rand(1, 200). "</show_id>\n";
+      $response .= "<title>". $title . "</title>\n";
+      $response .= "<time>". sprintf("%02d",rand(14, 23)) .":" . sprintf("%02d",rand(0, 3)*15) . "</time>\n";
+      $response .= "</show>\n";
+    }
+    $response .= "</list_of_shows>\n";
+    $s->add(new RiddlParameterComplex("out", "text/xml", $response));
+  }
+  if(strcasecmp($_SERVER['REQUEST_METHOD'], "POST") == 0) { # A reservation is made
+  }
+  $s->riddl_it(200);
+?>
