c0f8452a3d21746e0ad0aa8cd77b8cff0cd84312
Ralph Vigne <ralph@vigne.at>
2010-05-19 22:20:13 +0200

    RESCUEv0.2: extended testservice with reservation function

--- examples/rescue/Testservices/Cinema.php
+++ examples/rescue/Testservices/Cinema.php
@@ -2,11 +2,8 @@
   require_once("./serversimple.php");
   $s = new RiddlServerSimple();
   $params = $s->request();
-#  print_r($params);
-#  error_log(print_r($_SERVER, 1));
   if(strcasecmp($_SERVER['REQUEST_METHOD'], "GET") == 0) { # A list of shows is returned 
     # Expected Parameters (in order): title, datei, prefix_id
-#error_log(print_r($params, 1));
     foreach($params as $p) {
       if($p->name() == "title")
         $title = $p->value();
@@ -15,9 +12,6 @@
       if($p->name() == "pre")
         $prefix = $p->value();
     }
-#error_log("Title: " . $title);
-#error_log("Date: " . $date);
-#error_log("Prefix: " . $prefix);
     if($title == "") $title = "not given";
     if($date == "") $date = "not given";
     if($prefix == "") $prefix = "not given";
@@ -32,10 +26,16 @@
       $response .= "</show>";
     }
     $response .= "</list_of_shows>";
-error_log("Response: " . $response);    
     $s->add(new RiddlParameterComplex("out", "text/xml", $response));
   }
   if(strcasecmp($_SERVER['REQUEST_METHOD'], "POST") == 0) { # A reservation is made
+    foreach($params as $p) {
+      if($p->name() == "showID")
+        $showID = $p->value();
+    }
+    $pre = explode("_", $showID);
+    $resID = $pre[0] . "_" . rand(1, 200);
+    $s->add(new RiddlParameterSimple("resID", $resID));
   }
   $s->riddl_it(200);
 ?>
