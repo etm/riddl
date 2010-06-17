6040b840f9967f9a844b86a80c492fdc38fcdd67
Ralph Vigne <ralph@vigne.at>
2010-06-17 09:25:07 +0200

    RESCUEv0.2: got Kesselhaus-Cinema to work and started AugeGottes Cinema

--- examples/rescue/test.rb
+++ examples/rescue/test.rb
@@ -1,10 +1,14 @@
 require '../../lib/ruby/client.rb'
 require 'rubygems'
 require 'xml/smart'
+require 'cgi'
 
-      endpoint = "http://www.kinoimkesselhaus.at/programm/programm-kino"
+      endpoint = "http://www.cineplexx.at/content/kinos/kinos_programme.aspx"
       client = Riddl::Client.new(endpoint)
-      status, resp = client.get
+      status, resp = client.get [Riddl::Parameter::Simple.new("id", 6)]
       puts status
       puts resp.inspect
-      puts resp.value('').read
+      str = CGI::unescapeHTML(resp.value('').read)
+      str.gsub!('&', '&amp;')
+      xml = XML::Smart.string(str)
+      puts xml
