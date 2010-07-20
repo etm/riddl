8fa058d8ee1f2e6ca04eb29308b35bbc5e238c22
Ralph Vigne <ralph@vigne.at>
2010-07-20 10:47:56 +0200

    RESCUEv0.2: handler bug - not always receiving all notifications

--- examples/rescue/lib/InjectionHandler.rb
+++ examples/rescue/lib/InjectionHandler.rb
@@ -2,7 +2,7 @@ require '../../lib/ruby/client'
 
 class InjectionHandler < Riddl::Implementation
   $injection_queue = Hash.new
-  $notification_keys = Array.new
+  $notification_keys = Hash.new
   $registration_semaphore = Mutex.new # this semaphore prevents timing issues when a call leades to an injection which is directly followed by a sync_after event
   $received_stop = Hash.new
 
@@ -32,59 +32,82 @@ class InjectionHandler < Riddl::Implementation
         cpee_pos.add(ap.name.name, ap.text)
       end
       queue = xml.root.add('queued')
-      @p.value('instance')[-1..-1] == '/' ? inst = @p.value('instance').chop : inst = @p.value('instance')
-      $injection_queue[inst].each do |position, v|
+      $injection_queue[@p.value('instance')].each do |position, v|
         queue.add(position.to_s, v.to_s)
-      end if $injection_queue.key?(inst) 
+      end if $injection_queue.key?(@p.value('instance')) 
       callbacks = xml.root.add('outstanding-callbacks')
       $notification_keys.each {|v| callbacks.add('callback', {'key'=>v})}
       Riddl::Parameter::Complex.new("bla","text/xml", xml.to_s)
 # }}}
     elsif @p.value('instance') && @p.value('actual-position') && @p.value('new-position') # received list pending innjections for a given instance {{{
+pust "================= CALLED WIERED FUNCTION?"
        $injection_queue[@p.value('instance')][@p.value('new-position')] = $injection_queue[@p.value('instance')][@p.value('actual-position')]
        $injection_queue[@p.value('instance')].delete(@p.value('actual-position'))
 # }}}
     elsif @p.value('vote') == "syncing_after" && @p.value('topic') == "running"# received notification for sync_after {{{
-      registered = nil
-      $registration_semaphore.synchronize { registered = $notification_keys.include?(@p.value('key')) }
-      if registered
-        $notification_keys.delete(@p.value('key'))
-        status, resp = cpee.resource("notifications/subscriptions/#{@p.value('key')}").delete [
-          Riddl::Parameter::Simple.new("message-uid","ralph"),
-          Riddl::Parameter::Simple.new("fingerprint-with-producer-secret",Digest::MD5.hexdigest("ralph42"))
-        ]
-        puts "Injection-handler: ERROR deleting subscription (#{status})" unless status == 200 # Needs to be logged into the CPEE as well 
-        unless $injection_queue.include?(notification[:instance])
-          $injection_queue[notification[:instance]] = Hash.new
-          status, resp = cpee.resource("notifications/subscriptions").post [
-            Riddl::Parameter::Simple.new("url", "http://#{@env['HTTP_HOST']}#{@env['PATH_INFO']}"),
-            Riddl::Parameter::Simple.new("topic", "properties/state"),
-            Riddl::Parameter::Simple.new("events", "change")
-          ]
-          puts "Injection-handler: ERROR subscribing state/changed (#{status})" unless status == 200 # Needs to be logged into the CPEE as well
+      registered = false
+      $registration_semaphore.synchronize { 
+  puts "#{notification[:instance]} => #{notification[:activity]}"
+  p $notification_keys[notification[:instance]]
+  p @p.value('key')
+        $notification_keys[notification[:instance]].each do |e| 
+        p "a"
+        p e
+          if e[0] == @p.value('key') && e[1] == notification[:activity].to_s 
+            registered = true
+            $notification_keys[notification[:instance]].delete(e)
+          end
         end
-        $injection_queue[notification[:instance]][notification[:activity]] = 'at'
-        Riddl::Parameter::Simple.new('continue','false')
-      else
-        Riddl::Parameter::Simple.new('continue','true')
-      end
-# }}}
+  puts "reg #{registered}"
+        if registered
+          status, resp = cpee.resource("notifications/subscriptions/#{@p.value('key')}").delete [
+            Riddl::Parameter::Simple.new("message-uid","ralph"),
+            Riddl::Parameter::Simple.new("fingerprint-with-producer-secret",Digest::MD5.hexdigest("ralph42"))
+          ]
+          puts "Injection-handler: ERROR deleting subscription (#{status})" unless status == 200 # Needs to be logged into the CPEE as well 
+          unless $injection_queue.include?(notification[:instance])
+            $injection_queue[notification[:instance]] = Hash.new
+            status, resp = cpee.resource("notifications/subscriptions").post [
+              Riddl::Parameter::Simple.new("url", "http://#{@env['HTTP_HOST']}#{@env['PATH_INFO']}"),
+              Riddl::Parameter::Simple.new("topic", "properties/state"),
+              Riddl::Parameter::Simple.new("events", "change")
+            ]
+            puts "Injection-handler: ERROR subscribing state/changed (#{status})" unless status == 200 # Needs to be logged into the CPEE as well
+          end
+          $injection_queue[notification[:instance]][notification[:activity]] = 'at'
+          Riddl::Parameter::Simple.new('continue','false')
+        else
+          Riddl::Parameter::Simple.new('continue','true')
+        end
+      }
+# }}}  
     elsif @p.value('event') == "change" && @p.value('topic') == "properties/state"# received notification for instance stopped{{{
       unless notification[:state] != :stopped
-        $registration_semaphore.synchronize { 
+#        $registration_semaphore.synchronize { 
           return if $received_stop.include?(notification[:instance]) 
           $received_stop[notification[:instance]] = true 
-        }
+#        }
         status, resp = cpee.resource("notifications/subscriptions/#{@p.value('key')}").delete [
           Riddl::Parameter::Simple.new("message-uid","ralph"),
           Riddl::Parameter::Simple.new("fingerprint-with-producer-secret",Digest::MD5.hexdigest("ralph42"))
         ]
         puts "Injection-handler: ERROR deleting subscription (#{status})" unless status == 200 # Needs to be logged into the CPEE as well 
         changed_positions = Array.new
+        retries = 0
+        while $notification_keys[notification[:instance]].length > 0  && retries < 10 do
+            puts "Waiting -> Retrie #{retries}"; 
+            p $notification_keys[notification[:instance]]
+            sleep 0.5; 
+            retries += 1
+        end
+        if retries > 9
+          $notification_keys.delete(notification[:instance])
+          @status = 500
+          return
+        end
         queue = $injection_queue[notification[:instance]].dup
-        # $injection_queue[notification[:instance]].each do |position, state|
         queue.each do |position, state|
-          changed_positions.each { |p| position = p[:new] if position.to_s == p[:old]}
+          changed_positions.each { |p| position = p[:new] if position.to_s == p[:old].to_s}
           status, resp = Riddl::Client.new(injection_service_uri).post [
             Riddl::Parameter::Simple.new('position', position),
             Riddl::Parameter::Simple.new('instance', notification[:instance]),
@@ -95,7 +118,6 @@ class InjectionHandler < Riddl::Implementation
             xml = XML::Smart.string(resp.value('positions').read)
             xml.find('/positions/*').each do |p|
               changed_positions << {:old => p.name.name, :new => p.attributes['new'], :state => p.text}
-              $injection_queue[notification[:instance]][p.attributes['new'].to_sym] = 'at' if $injection_queue[notification[:instance]].key?(p.name.name.to_sym)
               $injection_queue[notification[:instance]].delete(p.name.name.to_sym)
             end
           end
@@ -116,9 +138,13 @@ class InjectionHandler < Riddl::Implementation
         $received_stop.delete(notification[:instance])
       end
 # }}} 
-    elsif @p.value('notification-key' ) # received subsription for sync_after {{{
-      $registration_semaphore.synchronize { $notification_keys << @p.value('notification-key') }
-      puts "\t=== Injection-handler: Receiving key of sync_after notification #{@p.value('notification-key')}" # without this puts the call back may come to fast will not be subscribed then      
+    elsif @p.value('notification-key') && @p.value('activity') && @p.value('instance') # received subsription for sync_after {{{
+      $registration_semaphore.synchronize { 
+      puts "\t=== Injection-handler: Receiving key of sync_after notification #{@p.value('notification-key')}" # without this puts the call back may come to fast will not be subscribed then => schould be sold when subscription on behalf is impĺemented      
+        $notification_keys[@p.value('instance')] = Array.new unless $notification_keys.key?(@p.value('instance'))
+        $notification_keys[@p.value('instance')] << [@p.value('notification-key'), @p.value('activity')] 
+        p $notification_keys[@p.value('instance')]
+      }
     # }}}
     else # some other request
       puts "\t=== Injection-handler: ERROR: unkonwn request"
--- examples/rescue/start.sh
+++ examples/rescue/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+reset
 rackup server.ru -p 9290
