b6942f6dadfb7edddd1c51ef5245a52ea54e96a5
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2011-11-25 02:16:54 +0100

    downloadify

---
+++ lib/ruby/utils/downloadify.rb
@@ -0,0 +1,30 @@
+require 'mime/types'
+require 'digest/md5'
+
+module Riddl
+  module Utils
+    class Downloadify < Riddl::Implementation
+      def response
+        path = File.file?(@a[0]) ? @a[0] : "#{@a[0]}/#{@r[@match.length-1..-1].join('/')}".gsub(/\/+/,'/')
+        if File.directory?(path)
+          @status = 404
+          return []
+        end
+        if File.exists?(path)
+          mtime = File.mtime(path)
+          @headers << Riddl::Header.new("Last-Modified",mtime.httpdate)
+          @headers << Riddl::Header.new("ETag",Digest::MD5.hexdigest(mtime.httpdate))
+          htime = @env["HTTP_IF_MODIFIED_SINCE"].nil? ? Time.at(0) : Time.parse(@env["HTTP_IF_MODIFIED_SINCE"])
+          if htime == mtime
+            @headers << Riddl::Header.new("Connection","close")
+            @status = 304 # Not modified
+            return []
+          else 
+            return Riddl::Parameter::Complex.new("file",MIME::Types.type_for(path).first.to_s,File.open(path,'r'))
+          end  
+        end
+        @status = 404
+      end  
+    end
+  end  
+end  
