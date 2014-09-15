dc8b38338f745e7d8176529d2db5e3315d026fe3
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2014-09-15 16:36:34 +0200

    shared escape/unescape finish

--- lib/ruby/riddl/protocols/utils.rb
+++ lib/ruby/riddl/protocols/utils.rb
@@ -6,16 +6,16 @@ module Riddl
       # query strings faster.  Use this rather than the cgi.rb
       # version since it's faster. (%20 instead of + for improved standards conformance).
       def self.escape(s)
-        s.to_s.gsub(/([^a-zA-Z0-9_.-]+)/n) {
+        s.to_s.dup.force_encoding('ASCII-8BIT').gsub(/([^a-zA-Z0-9_.-]+)/n) {
           '%'+$1.unpack('H2'*$1.size).join('%').upcase
         }
       end
 
       def self::unescape(s)
-        return s if s.nil?  
-        s.tr('+', ' ').gsub(/((?:%[0-9a-fA-F]{2})+)/n){
+        return s if s.nil?                                                                                                                                                                                       
+        s.force_encoding("ASCII-8BIT").tr('+', ' ').gsub(/((?:%[0-9a-fA-F]{2})+)/n){
           [$1.delete('%')].pack('H*')
-        }
+        }.force_encoding('UTF-8')
       end
 
     end
