e73cbceddea20703c804b8e30973931afa8dc34f
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-06-08 23:00:05 +0200

    relaxng handler problem; release

--- lib/ruby/riddl/handlers/relaxng.rb
+++ lib/ruby/riddl/handlers/relaxng.rb
@@ -2,17 +2,22 @@ module Riddl
   module Handlers
     class RelaxNG < Riddl::Handlers::Implementation
       def self::handle(what,hinfo)
-        w = if what.class == Riddl::Parameter::Tempfile || what.class == File
-          XML::Smart.open_unprotected(what)
-        else  
-          XML::Smart.string(what)
-        end  
-        rng = XML::Smart.string("<payload>" + hinfo + "</payload>")
-        rng.register_namespace 'r', 'http://relaxng.org/ns/structure/1.0'
-        w.validate_against(rng.find("//r:grammar|//r:element").first.to_doc) rescue false
+        begin
+          w = if what.class == Riddl::Parameter::Tempfile || what.class == File
+            XML::Smart.open_unprotected(what)
+          else
+            XML::Smart.string(what)
+          end
+          rng = XML::Smart.string("<payload>" + hinfo + "</payload>")
+          rng.register_namespace 'r', 'http://relaxng.org/ns/structure/1.0'
+          w.validate_against(rng.find("//r:grammar|//r:element").first.to_doc) rescue false
+        rescue
+          puts 'The XML sent to the server is b0rked/>'
+          false
+        end
       end
     end
-  end  
-end  
+  end
+end
 
 Riddl::Handlers::add("http://riddl.org/ns/handlers/relaxng",Riddl::Handlers::RelaxNG)
