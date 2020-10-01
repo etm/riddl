f6bcc31d2e7794c926ac66b98e73d19f0afe6812
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-10-01 11:58:06 +0200

    handler text

--- lib/ruby/riddl/handlers/plain-type.rb
+++ lib/ruby/riddl/handlers/plain-type.rb
@@ -2,20 +2,22 @@ module Riddl
   module Handlers
     class PlainType
       def self::handle(what,hinfo)
+        p hinfo
         if what.class == Riddl::Parameter::Tempfile
           w = what.read
-        else  
+        else
           w = what
-        end  
+        end
         begin
-          re = Regexp.new(hinfo)
-        rescue  
+          hi = XML::Smart::string(hinfo)
+          re = Regexp.new(hi.root.text)
+        rescue => e
           return false
         end
         w =~ re
       end
     end
-  end  
-end  
+  end
+end
 
 Riddl::Handlers::add("http://riddl.org/ns/handlers/plain-type",Riddl::Handlers::PlainType)
