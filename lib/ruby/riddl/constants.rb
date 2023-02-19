be0e8f0947da0e2a5ca1bdc23be265142b92c709
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2023-02-19 01:33:10 +0100

    frozen constants; release

--- lib/ruby/riddl/constants.rb
+++ lib/ruby/riddl/constants.rb
@@ -1,5 +1,5 @@
 module Riddl
-  BOUNDARY = "Time_is_an_illusion._Lunchtime_doubly_so.0xriddldata"
-  EOL = "\r\n"
-  D = '&;'
-end  
+  BOUNDARY = 'Time_is_an_illusion._Lunchtime_doubly_so.0xriddldata'.freeze
+  EOL = "\r\n".freeze
+  D = '&;'.freeze
+end
